80b2a1218cf3cf4c36d75476c23111ef112209f3
Raikoff <podchernin.ivan@gmail.com>
2022-01-27 11:20:57 +0300

    добавлен код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] myArray = new string[3];
+Console.Write("Введите данные №1: ");
+string a = Console.ReadLine();
+Console.Write("Введите данные №2: ");
+string b = Console.ReadLine();
+Console.Write("Введите данные №3: ");
+string c = Console.ReadLine();
+myArray[0] = a;
+myArray[1] = b;
+myArray[2] = c;
+
+for(int i = 0; i < 3; i++)
+    {
+       if(myArray[i].Length < 4 && myArray[i].Length > 0)
+       {
+        Console.WriteLine(myArray[i]);
+       }
+    }
+
+
